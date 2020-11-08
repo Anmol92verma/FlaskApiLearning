ced94d4dff3a0c3254ed855023ee3bbe625a6bbd
anmol.verma4@gmail.com <Anmol Verma>
2020-11-08 15:41:43 +0530

    connected with mysql database

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask
+
+from database import mysql
 from routes.Users import userBlueprint
 from routes.Posts import postBlueprint
 
 flaskApp = Flask(__name__)
 flaskApp.register_blueprint(userBlueprint)
 flaskApp.register_blueprint(postBlueprint)
+
+
+flaskApp.config['MYSQL_DATABASE_USER'] = "root"
+flaskApp.config['MYSQL_DATABASE_PASSWORD'] = "helloworld"
+flaskApp.config["MYSQL_DATABASE_DB"] = "db_fake"
+
+mysql.init_app(flaskApp)
 
 comments = []
 
--- routes/Users.py
+++ routes/Users.py
@@ -1,25 +1,25 @@
 from flask import request, jsonify, Blueprint
 
-userBlueprint = Blueprint('userBlueprint', __name__)
+from database import mysql
 
-users = []
+userBlueprint = Blueprint('userBlueprint', __name__)
 
 
 @userBlueprint.route('/signup')
 def create_user():
     name = request.json['name']
     uname = request.json['username']
-    password = request.json['password']
-    if len(name) == 0 or len(uname) == 0 or len(password) == 0:
-        return jsonify({'status': 201, 'message': "Check the validations"})
+    id = request.json['id']
 
-    result = list(filter(lambda user: (user['username'] == uname), users))
-    if len(result) == 0:
-        users.append({
-            'name': name,
-            'username': uname,
-            'password': password
-        })
-        return jsonify({'status': 200, 'message': "User Created"})
-    else:
+    conn = mysql.get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM employee where email = %s", uname)
+    data = cursor.fetchall()
+    if cursor.rowcount == 1:
         return jsonify({'status': 201, 'message': "User already exist"})
+    else:
+        sql = "INSERT INTO employee (id, email,name) VALUES (%s, %s,%s)"
+        val = (id, uname, name)
+        cursor.execute(sql, val)
+        conn.commit()
+        return jsonify({'status': 200, 'message': "User Created"})
